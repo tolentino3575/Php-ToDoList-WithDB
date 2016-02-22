b9ad2243f3d55c6310752bb66508eab5df5432c6
erik tolentino and michael lor <tolentino3575@gmail.com>
2016-02-22 11:42:15 -0800

    apply database functionality

---
+++ app/app.php
@@ -0,0 +1,53 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/task.php";
+    require_once __DIR__."/../src/Category.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost;dbname=to_do';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/tasks", function() use ($app) {//all current tasks page
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
+
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/categories", function() use ($app){
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/delete_categories", function() use ($app){
+        Category::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->post("/tasks", function() use ($app) {//create task page
+        $task = new Task($_POST['description']);
+        $task->save();
+        return $app['twig']->render('tasks.html.twig', array('newtask' => Task::getAll()));
+    });
+
+    $app->post("/delete_tasks", function() use ($app) {
+        Task::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
+    return $app;
+?>
---
+++ src/task.php
@@ -0,0 +1,65 @@
+<?php
+class Task
+{
+    private $description;
+    private $id;
+
+    function __construct($description, $id = null)
+    {
+        $this->description=$description;
+        $this->id = $id;
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+
+    function setDescription($new_description)
+    {
+        $this->description = (string) $new_description;
+    }
+
+    function getDescription()
+    {
+        return $this->description;
+    }
+
+    function save()
+    {
+        $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+        $this->id = $GLOBALS['DB']->lastInsertId();
+    }//allows you to save tasks in the superglobal variable
+
+    static function getAll()
+    {
+        $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+        $tasks = array();
+        foreach($returned_tasks as $task) {
+            $description = $task['description'];
+            $id = $task['id'];
+            $new_task = new Task($description, $id);
+            array_push($tasks, $new_task);
+        }
+        return $tasks;
+    }//static functions are getters. its job is to return the list of all of our tasks.
+
+    static function deleteAll()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM tasks;");
+    }
+
+    static function find($search_id)
+    {
+        $found_task = null;
+        $tasks = Task::getAll();
+        foreach($tasks as $task) {
+            $task_id = $task->getId();
+            if ($task_id == $search_id) {
+                $found_task = $task;
+            }
+        }
+        return $found_task;
+    }
+}
+?>
